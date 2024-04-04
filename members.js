8c38e03877fd504429561529a5668f3254b11bbc
Amir J <amir.jaafar@neos-sdi.com>
2024-04-04 18:10:12 +0200

    2e commit copilot

---
+++ members.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: 'John Doe',
+        age: 30,
+        skills: ['HTML', 'CSS', 'JS'],
+        address: {
+            city: 'New York',
+            country: 'USA'
+        }
+    };
+    return member;
+}
--- skills.js
+++ skills.js
@@ -1,3 +1,18 @@
 function calculateSubstractionNumbers(var1, var2) {
     return var1 - var2;
 }
+function calculateAdditionNumbers(var1, var2) {
+    return var1 + var2;
+}
+
+function calculateMultiplicationNumbers(var1, var2) {
+    return var1 * var2;
+}
+
+function calculateDivisionNumbers(var1, var2) {
+    return var1 / var2;
+}
+
+function calculateModuloNumbers(var1, var2) {
+    return var1 % var2;
+}
